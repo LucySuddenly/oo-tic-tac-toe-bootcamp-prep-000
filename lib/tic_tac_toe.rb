4347eece842af98892a15be8331b55825b3dd3ee
Lucy Suddenly <suddenlylucy@gmail.com>
2018-11-13 20:48:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -95,5 +95,14 @@ class TicTacToe
       winning_combo
     end
   end
-  
+  def play
+    until over? 
+      turn
+    end
+    if won?
+     puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cat's Game!"
+    end 
+  end
 end 
